d5d330e385c5bca60c06f62b51e462396f546ac2
erismaster <dcampau1@gmail.com>
2013-01-30 22:40:59 -0500

    Lab4
    Fixed lab4.cpp

--- Console Apps/Source code/Labs/Lab4.cpp
+++ Console Apps/Source code/Labs/Lab4.cpp
@@ -1,4 +1,4 @@
-// temperature.cpp:��
+// temperature.cpp
 // Author: Derrik Campau
 // Date: 1/30/13
 // Compiler: Microsoft Visual C++ .NET
@@ -11,19 +11,21 @@ int main()
 {
 	double body_temperature;
 
-	cout << "Your body temperature: " << endl; // prompts for user input
+	cout << "Your body temperature: "; // prompts for user input
 
 	cin >> body_temperature; //read input from user
 
+	cout << endl;
+
 	if (body_temperature >= 98.6) 
 	
-		cout << "You probably have a fever";
+		cout << "You probably have a fever\n";
 	
 	else if ((body_temperature < 98.6) && (body_temperature >= 96.0))
 	
-		cout<< "You have a normal temperature";
+		cout<< "You have a normal temperature\n";
 	else
-		cout << "I cannot tell, please see your doctor";
+		cout << "I cannot tell, please see your doctor\n";
 
 	return 0;
 
