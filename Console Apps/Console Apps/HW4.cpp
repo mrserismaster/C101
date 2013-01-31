a10e46f6f33bf51ed444de8bfcf7d27b09bbb7f5
erismaster <dcampau1@gmail.com>
2013-01-31 10:14:03 -0500

    Init Homework 4

---
+++ Console Apps/Console Apps/HW4.cpp
@@ -0,0 +1,23 @@
+// HW4.cpp
+// Author: Derrik Campau
+// Date: 1/31/13
+// Compiler: Microsoft Visual C++ .NET
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+	double score1, score2, score3;
+
+
+
+
+
+
+
+	return 0;
+
+}
