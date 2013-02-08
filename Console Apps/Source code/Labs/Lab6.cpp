63bde52345e1e8c657bf7a202cb142e430b84902
erismaster <dcampau1@gmail.com>
2013-02-08 16:12:53 -0500

    Started Lab6

--- Console Apps/Source code/Labs/Lab6.cpp
+++ Console Apps/Source code/Labs/Lab6.cpp
@@ -2,3 +2,21 @@
 // Author: Derrik Campau
 // Date: 2/7/13
 // Compiler: Microsoft Visual C++ .NET
+
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+	int m, n;
+
+	for (m=0; m<10; m++)
+	{
+		for (n=0; n<m; n++) cout << " ";
+		for (n=0; n<(19-2*m); n++) cout << "*";
+		for (n=0; n<m; n++) cout << " ";
+		cout << endl;
+	}
+
+		return 0;
+}
