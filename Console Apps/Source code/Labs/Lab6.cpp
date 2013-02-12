718b58bfc8cf80e0453dee3bc715c8068f4e8f44
erismaster <dcampau1@gmail.com>
2013-02-11 21:41:12 -0500

    Finished Lab6

--- Console Apps/Source code/Labs/Lab6.cpp
+++ Console Apps/Source code/Labs/Lab6.cpp
@@ -1,26 +1,53 @@
-// Lab6.cpp
+﻿// Lab6.cpp
 // Author: Derrik Campau
 // Date: 2/7/13
 // Compiler: Microsoft Visual C++ .NET
 
-#include<iostream>
+#include <iostream>
 using namespace std;
+/*        
 
+Original code
+
+int main ()
+{
+int m, n;
+for (m=0; m<10; m++)
+{
+	for (n=0; n<m; n++) cout << " ";
+	for (n=0; n<(19-2*m); n++) cout << "*";
+	for (n=0; n<m; n++) cout << " ";
+	cout << endl;
+}
+return 0;
+}
+*/
+
+//inverse of above
 int main ()
 {
 	int m, n;
 
-	for (m=10; m>0; m--)
+	for (m=10; m>=0; m--)
 	{
-		//for (n=10; n>m; n--) cout << " ";
-		//for (n=10; n>(19-2*m); n--) cout << "*";
-		//for (n=10; n>m; n--) cout << " ";
-
 		for (n=0; n<m; n++) cout << " ";
-		for (n=0; n<(19-2*m); n++) cout << "*";
+		for (n=0; n<(21-2*m); n++) cout << "*";
 		for (n=0; n<m; n++) cout << " ";
 		cout << endl;
 	}
-
 		return 0;
 }
+
+/*
+          *
+         ***
+        *****
+       *******
+      *********
+     ***********
+    *************
+   ***************
+  *****************
+ *******************
+*********************
+*/
