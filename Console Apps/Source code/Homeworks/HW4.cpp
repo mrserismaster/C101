060db25faa1279ad369c79b3b99b7292a6ceca00
Derrik Campau <dcampau1@gmail.com>
2013-02-19 17:21:10 -0500

    Cleaned up

--- Console Apps/Source code/Homeworks/HW4.cpp
+++ Console Apps/Source code/Homeworks/HW4.cpp
@@ -31,8 +31,6 @@ int main()
 		return 0;
 	}
 
-
-	
 	cout << endl
 		 << "--------------------------------\n"
 		 << "Student ID: ";
@@ -63,13 +61,109 @@ int main()
 
 	cout << "--------------------------------\n"
 		 << "Student ID		Grade (%)		Final Grade\n"
-		 << id << "			" << grade << "			" << letter << endl;
+		 << id << "			" << grade << "			" << letter << endl
+		 << endl
+		 << endl;
 	}
 
 	if (choice == 2)
 		exit;
 
-
 	return 0;
 
 }
+
+/*
+*****************************************
+Welcome to Grade Book. Please select one of 2 options
+1. Enter the student score and determine grade
+2. Exit the program
+*****************************************
+Enter Selection: 1
+
+--------------------------------
+Student ID: 1
+Exam 1 score: 100
+Exam 2 score: 90
+Exam 3 score: 95
+--------------------------------
+Student ID              Grade (%)               Final Grade
+1                       95                      A
+
+
+*****************************************
+Welcome to Grade Book. Please select one of 2 options
+1. Enter the student score and determine grade
+2. Exit the program
+*****************************************
+Enter Selection: 1
+
+--------------------------------
+Student ID: 2
+Exam 1 score: 88
+Exam 2 score: 80
+Exam 3 score: 75
+--------------------------------
+Student ID              Grade (%)               Final Grade
+2                       81                      B
+
+
+*****************************************
+Welcome to Grade Book. Please select one of 2 options
+1. Enter the student score and determine grade
+2. Exit the program
+*****************************************
+Enter Selection: 1
+
+--------------------------------
+Student ID: 3
+Exam 1 score: 77
+Exam 2 score: 77
+Exam 3 score: 64
+--------------------------------
+Student ID              Grade (%)               Final Grade
+3                       72.6667                 C
+
+
+*****************************************
+Welcome to Grade Book. Please select one of 2 options
+1. Enter the student score and determine grade
+2. Exit the program
+*****************************************
+Enter Selection: 1
+
+--------------------------------
+Student ID: 4
+Exam 1 score: 60
+Exam 2 score: 60
+Exam 3 score: 60
+--------------------------------
+Student ID              Grade (%)               Final Grade
+4                       60                      D
+
+
+*****************************************
+Welcome to Grade Book. Please select one of 2 options
+1. Enter the student score and determine grade
+2. Exit the program
+*****************************************
+Enter Selection: 1
+
+--------------------------------
+Student ID: 5
+Exam 1 score: 0
+Exam 2 score: 50
+Exam 3 score: 75
+--------------------------------
+Student ID              Grade (%)               Final Grade
+5                       41.6667                 F
+
+
+*****************************************
+Welcome to Grade Book. Please select one of 2 options
+1. Enter the student score and determine grade
+2. Exit the program
+*****************************************
+Enter Selection: 2
+GoodBye!
+*/
