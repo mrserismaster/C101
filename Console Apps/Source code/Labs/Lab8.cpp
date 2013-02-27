e58863a6cae83a0e40fa3d3a4d35f2288b13571b
erismaster <dcampau1@gmail.com>
2013-02-27 15:12:10 -0500

    Lab8

--- Console Apps/Source code/Labs/Lab8.cpp
+++ Console Apps/Source code/Labs/Lab8.cpp
@@ -1,29 +1,33 @@
-//Your name: Derrik Campau
+// Name: Derrik Campau
+// Lab8
+// 2/27/2013
 
 #include <iostream> 
 #include <cmath>
 
 using namespace std;
 
-int determine_day_of_week (int day, int month, int year); 
+int determine_day_of_week (int day, int month, int year);
 
 int main() 
 { 
 
 	int day, month, year; 
 	int result; 
-	
 	cout << "Enter a date " << endl; 
 	cout << "month: "; 
 	cin >> month; 
+	
 	cout << "day: "; 
 	cin >> day; 
+	
 	cout << "year: "; 
 	cin >> year; 
 	
 	result = determine_day_of_week(day, month, year); 
-	cout << month << "-" << day << "-" << year << " is "; 
 	
+	cout << month << "-" << day << "-" << year << " is "; 
+      
 	if (result==0) cout << "Sunday"; 
 	else if (result == 1) cout << "Monday"; 
 	else if (result == 2) cout << "Tuesday"; 
@@ -33,12 +37,38 @@ int main()
 	else cout << "Saturday"; 
 	
 	cout << endl; 
-
+	
 	return 0; 
 } 
-
 int determine_day_of_week(int day, int month, int year) 
 { 
- //Your code goes here 
- 
-} 
+ int weekDay, c, y, s;
+	if ((year % 100 == 0) && (month == 1))
+	{
+		c = (year / 100) -1;
+		y = (year % 100) + 99;
+		month = month + 10;
+	}
+	else if ((year % 100 == 0) && (month == 2))
+	{
+		c = (year / 100) -1;
+		y = (year % 100) + 99;
+		month = month + 10;
+	}
+	else if ((month == 1) || (month == 2)) 
+	{
+		c = year / 100;
+		y = (year % 100) - 1;
+		month = month + 10;
+	}
+	else 
+	{
+		c = year / 100;
+		y = year % 100;
+		month = month - 2;
+	}
+	s = (day + floor(2.6*month - 0.2) - 2*c + y + floor(y/4.0) + floor(c/4.0));
+	weekDay = s % 7;
+	cout << day << " " << weekDay << " " << y << " " << c << " " << s << endl;
+	return (weekDay);
+}
