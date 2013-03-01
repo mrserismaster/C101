edfdf12580c657d47a6f59c5c8067b5e7d8ef283
erismaster <dcampau1@gmail.com>
2013-02-28 20:04:53 -0500

    Finished HW 6

--- Console Apps/Source code/Homeworks/HW6.cpp
+++ Console Apps/Source code/Homeworks/HW6.cpp
@@ -1,3 +1,37 @@
 // HW6.cpp
 // 2.28.2013
 // Derrik Campau
+// This program will add the digits of the number entered by the user
+
+#include <iostream>
+#include <cmath>
+
+int digitSumCalc(int);
+
+using namespace std;
+
+int main() {
+
+	int input, result;
+
+	cout << "Please enter a positive integer: ";
+	cin >> input;
+
+	result = digitSumCalc(input);		//calls the digitsum function
+
+	cout << "The digit sum of the number you entered is: " << result << endl;
+
+	return 0;
+}
+
+int digitSumCalc(int number) {
+
+	int sum = 0;
+
+	while ( number > 0 ) {
+	sum += number % 10;
+	number /= 10;
+	}
+
+	return sum;
+}
